7401cd210eade8f8d1c9b3f6fd74fd2c9df88c96
lightningflashh <thanhchi0429@gmail.com>
2025-06-19 16:40:39 +0700

    init project #2

--- trello-web/vite.config.js
+++ trello-web/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // This is important for relative paths to work correctly in production
 })
