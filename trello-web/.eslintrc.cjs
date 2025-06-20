038278e1b64b13565ae7fb662c8fb928933d6c5a
lightningflashh <thanhchi0429@gmail.com>
2025-06-20 09:07:55 +0700

    config(file): update mui rules

--- trello-web/.eslintrc.cjs
+++ trello-web/.eslintrc.cjs
@@ -14,11 +14,20 @@ module.exports = {
     'react-refresh'
   ],
   rules: {
+    // React specific rules
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
+
+    // MUI specific rules
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
 
     'no-console': 1,
     'no-lonely-if': 1,
