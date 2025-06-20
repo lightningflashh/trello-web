af130f5cc4c3bfa3c65313a4cc6a712bfdbcafc4
lightningflashh <thanhchi0429@gmail.com>
2025-06-20 10:52:41 +0700

    config(theme): handle dark or light mode

--- trello-web/src/App.jsx
+++ trello-web/src/App.jsx
@@ -1,12 +1,30 @@
+import { useColorScheme } from '@mui/material'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+function ModeToogle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+    >
+      Switch to {mode === 'light' ? 'dark' : 'light'} mode
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToogle />
       <div>
         <h1>Click me!</h1>
       </div>
       <Button variant="contained">Hello world</Button>
+      <Typography variant="body2" color="text.secondary">
+        Hello world
+      </Typography>
     </>
   )
 }
