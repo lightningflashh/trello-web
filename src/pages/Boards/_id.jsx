5fe664bf74333d3770f276f25a3b7d8cdfb71db6
lightningflashh <thanhchi0429@gmail.com>
2025-07-01 11:27:00 +0700

    feat(api): integrate axios for fetching board details

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react'
+
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import Container from '@mui/material/Container'
-import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 const Board = () => {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // Simulate fetching board data
+    const boardId = '686352914b45dfbda0636f99'
+    fetchBoardDetailsAPI(boardId)
+      .then(board => {
+        setBoard(board)
+      })
+      .catch(error => {
+        console.error('Error fetching board details:', error)
+      })
+  }, [])
+
   return (
     <>
       <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
         <AppBar/>
-        <BoardBar board={mockData?.board}/>
-        <BoardContent board={mockData?.board}/>
+        <BoardBar board={board}/>
+        <BoardContent board={board}/>
       </Container>
     </>
   )
